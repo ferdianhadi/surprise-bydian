84b35ef4e3e507fb2692af60e7025a27401dc20c
ferdianhadi <ferdianhadi12345@gmail.com>
2025-03-25 23:24:38 +0700

    Initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,27 @@
+import bg from './bg.jpg'
+
+import picture from './picture.png'
+import pic1 from './pic1.jpeg'
+import pic2 from './pic2.jpeg'
+import pic3 from './pic3.jpeg'
+import pic4 from './pic4.jpeg'
+import pic5 from './pic5.jpeg'
+import pic6 from './pic6.jpeg'
+import confetti from './confetti.gif'
+
+import CakeSVG from './svg/CakeSVG'
+
+
+
+export {
+    bg,
+    picture,
+    confetti,
+    CakeSVG,
+    pic1,
+    pic2,
+    pic3,
+    pic4,
+    pic5,
+    pic6
+}
